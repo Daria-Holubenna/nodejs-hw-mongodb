21ec861b560e74369e50024e996a14971855974c
Daria H <dholubenna@gamil.com>
2025-09-12 13:41:48 +0300

    fix get funct. findById

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -6,6 +6,6 @@ export const getAllContacts = async () => {
 };
 
 export const getContactById = async (contactId) => {
-  const contacts = await ContactsCollection.findNyId(contactId);
+  const contacts = await ContactsCollection.findById(contactId);
   return contacts;
 };
